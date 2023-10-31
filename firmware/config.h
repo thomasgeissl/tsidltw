fb8617681802bf8a078b97431ab94418262f3014
Thomas Geissl <thomas.geissl@gmail.com>
2023-10-31 22:58:06 +0100

    added firmware

---
+++ firmware/config.h
@@ -0,0 +1,10 @@
+#define ID 1
+#define HAS_MIDI false
+#define HAS_MPU6050 true
+
+#define LOCAL_OSC_PORT 8000
+#define REMOTE_OSC_PORT 8088
+
+#define NUMBER_OF_TOUCHES 2
+//int touchPins[NUMBER_OF_TOUCHES] = {4, 2, 15, 32, 33, 27, 14, 12, 13};
+int touchPins[NUMBER_OF_TOUCHES] = {13,15};
---
+++ firmware/firmware.ino
@@ -0,0 +1,183 @@
+#include "config.h"
+#include <WiFi.h>
+#if HAS_MIDI
+#include <Adafruit_TinyUSB.h>
+#include <MIDI.h>
+#endif
+#include <OSCMessage.h>
+
+
+#if HAS_MPU6050
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
+
+Adafruit_MPU6050 mpu;
+#endif
+
+
+
+
+#if HAS_MIDI
+Adafruit_USBD_MIDI usb_midi;
+MIDI_CREATE_INSTANCE(Adafruit_USBD_MIDI, usb_midi, usbMIDI);
+#endif
+
+const char *Apssid = "nantlab-wlan";
+const char *Appassword = "nantlab-wlan-pw";
+WiFiUDP Udp;
+const IPAddress outIp(255, 255, 255, 255);
+
+int touchValues[NUMBER_OF_TOUCHES];
+sensors_event_t a, g, temp;
+
+long _timestamp;
+long _frameCounter = 0;
+
+void readSensors () {
+  for (auto i = 0; i < NUMBER_OF_TOUCHES; i++) {
+    touchValues[i] = touchRead(touchPins[i]);
+    Serial.println(touchValues[i]);
+  }
+
+  mpu.getEvent(&a, &g, &temp);
+
+  Serial.println(g.gyro.x);
+
+}
+
+void sendValues() {
+  OSCMessage touchMsg("/esp/touch");
+  touchMsg.add(ID);
+  for (auto i = 0; i < NUMBER_OF_TOUCHES; i++) {
+    touchMsg.add(touchValues[i]);
+  }
+  Udp.beginPacket(outIp, REMOTE_OSC_PORT);
+  touchMsg.send(Udp);
+  Udp.endPacket();
+  touchMsg.empty();
+
+  OSCMessage accelerometerMsg("/esp/motion");
+  accelerometerMsg.add(ID);
+  accelerometerMsg.add(g.gyro.x);
+  accelerometerMsg.add(g.gyro.y);
+  accelerometerMsg.add(g.gyro.z);
+  accelerometerMsg.add(a.acceleration.x);
+  accelerometerMsg.add(a.acceleration.y);
+  accelerometerMsg.add(a.acceleration.z);
+  Udp.beginPacket(outIp, REMOTE_OSC_PORT);
+  accelerometerMsg.send(Udp);
+  Udp.endPacket();
+  accelerometerMsg.empty();
+
+  OSCMessage temperatureMsg("/esp/temperature");
+  temperatureMsg.add(ID);
+  temperatureMsg.add(temp.temperature);
+
+  Udp.beginPacket(outIp, REMOTE_OSC_PORT);
+  temperatureMsg.send(Udp);
+  Udp.endPacket();
+  temperatureMsg.empty();
+
+
+#if HAS_MIDI
+  usbMIDI.sendControlChange(1, 1, ID);
+  usbMIDI.sendNoteOn(60, 127, ID);
+  delay(50);
+  usbMIDI.sendNoteOff(60, 0, ID);
+#endif
+}
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.mode(WIFI_AP_STA);
+
+  WiFi.softAP(Apssid, Appassword);
+  IPAddress myIP = WiFi.softAPIP();
+  Serial.print("Access Point IP address: ");
+  Serial.println(myIP);
+  Udp.begin(LOCAL_OSC_PORT);
+
+#if HAS_MIDI
+  usbMIDI.begin();
+#endif
+
+  // Try to initialize!
+  if (!mpu.begin()) {
+    Serial.println("Failed to find MPU6050 chip");
+    while (1) {
+      delay(10);
+    }
+  }
+  Serial.println("MPU6050 Found!");
+  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+  Serial.print("Accelerometer range set to: ");
+  switch (mpu.getAccelerometerRange()) {
+    case MPU6050_RANGE_2_G:
+      Serial.println("+-2G");
+      break;
+    case MPU6050_RANGE_4_G:
+      Serial.println("+-4G");
+      break;
+    case MPU6050_RANGE_8_G:
+      Serial.println("+-8G");
+      break;
+    case MPU6050_RANGE_16_G:
+      Serial.println("+-16G");
+      break;
+  }
+  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+  Serial.print("Gyro range set to: ");
+  switch (mpu.getGyroRange()) {
+    case MPU6050_RANGE_250_DEG:
+      Serial.println("+- 250 deg/s");
+      break;
+    case MPU6050_RANGE_500_DEG:
+      Serial.println("+- 500 deg/s");
+      break;
+    case MPU6050_RANGE_1000_DEG:
+      Serial.println("+- 1000 deg/s");
+      break;
+    case MPU6050_RANGE_2000_DEG:
+      Serial.println("+- 2000 deg/s");
+      break;
+  }
+
+  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+  Serial.print("Filter bandwidth set to: ");
+  switch (mpu.getFilterBandwidth()) {
+    case MPU6050_BAND_260_HZ:
+      Serial.println("260 Hz");
+      break;
+    case MPU6050_BAND_184_HZ:
+      Serial.println("184 Hz");
+      break;
+    case MPU6050_BAND_94_HZ:
+      Serial.println("94 Hz");
+      break;
+    case MPU6050_BAND_44_HZ:
+      Serial.println("44 Hz");
+      break;
+    case MPU6050_BAND_21_HZ:
+      Serial.println("21 Hz");
+      break;
+    case MPU6050_BAND_10_HZ:
+      Serial.println("10 Hz");
+      break;
+    case MPU6050_BAND_5_HZ:
+      Serial.println("5 Hz");
+      break;
+  }
+
+  Serial.println("");
+  delay(100);
+}
+
+void loop() {
+  auto timestamp = millis();
+  _frameCounter++;
+  readSensors();
+  if (_frameCounter % 2 == 0) {
+    sendValues();
+  }
+}
