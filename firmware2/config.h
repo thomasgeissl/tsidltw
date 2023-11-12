798910cfa3520b3b5f6c613a41498a9d5260ff8e
Blackistone <82961541+blackistone@users.noreply.github.com>
2023-11-12 14:54:00 +0100

    Add files via upload

---
+++ firmware2/config.h
@@ -0,0 +1,23 @@
+#define ID 1
+#define HAS_MIDI false
+#define HAS_MPU6050 true
+#define HAS_VL53L false
+
+#define LOCAL_OSC_PORT 8000
+#define REMOTE_OSC_PORT 8088
+
+#define NUMBER_OF_TOUCHES 2
+//int touchPins[NUMBER_OF_TOUCHES] = {4, 2, 15, 32, 33, 27, 14, 12, 13};
+int touchPins[NUMBER_OF_TOUCHES] = {13,15};
+
+#define SEND_INTERVAL_MS 100
+
+#define MAX_NUMBER_OF_TRIES 10
+
+#define AP_SSID "HH71VM_5BA1_2.4G"
+#define AP_PASSWORD "cocojumbo"
+
+#define NETWORK_SSID "HH71VM_5BA1_2.4G"
+#define PASSWORD "cocojumbo"
+//#define NETWORK_SSID "TODO"
+//#define PASSWORD "TODO"
